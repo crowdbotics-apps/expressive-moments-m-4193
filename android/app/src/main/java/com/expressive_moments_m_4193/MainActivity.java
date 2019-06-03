97c6f858a53e5f619b95228113a9e43e6404a63e
Crowdbotics <team@crowdbotics.com>
2019-06-03 17:13:07 +0000

    initial commit

---
+++ android/app/src/main/java/com/expressive_moments_m_4193/MainActivity.java
@@ -0,0 +1,15 @@
+package com.expressive_moments_m_4193;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "expressive_moments_m_4193";
+    }
+}
